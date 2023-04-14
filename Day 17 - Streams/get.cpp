f806c045255b22d754339fd4aa1b1c37da16e8d0
Boy-Bird <yashparihar44@gmail.com>
2023-04-14 16:10:23 +0530

    git()

---
+++ Day 17 - Streams/get.cpp
@@ -0,0 +1,11 @@
+// get() with no parameters
+#include <iostream>
+
+int main(){
+  char ch;
+  while( (ch = std::cin.get()) != EOF){
+    std::cout << "ch: " << ch << std::endl;
+  }
+  std::cout << "\nDone!\n";
+  return 0;
+}
